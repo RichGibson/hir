dd3de43189d0427614b6340cfb010e7cbb76a89b
Rich Gibson <rich.gibson@gmail.com>
2014-02-10 23:10:50 +0100

    working on viewing formentries

---
+++ test.py
@@ -0,0 +1,33 @@
+from django.core.management import setup_environ
+#from hir import settings
+import settings
+setup_environ(settings)
+
+from mezzanine.forms.models import *
+
+form_slug = 'organizationspace'
+single_form = Form.objects.get(slug=form_slug)
+formentry_list=FormEntry.objects.filter(form_id=single_form.id)
+label_list = [ ]
+field_list = Field.objects.filter(form_id=single_form.id)
+
+print "Fields in form titled '%s' " % single_form.title
+for field in field_list:
+    print field.id, field.label
+
+print 
+
+entrylist = [ ]
+
+# I want a list of formentries?
+# now I build a list of data entries
+for formentry in formentry_list:
+    fieldentry_list = FieldEntry.objects.filter(entry_id=formentry.id)
+    for fieldentry in fieldentry_list:
+        entrylist.append(fieldentry)
+
+for entry in entrylist:
+    print dir(entry)
+
+print entrylist
+
