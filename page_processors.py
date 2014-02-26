deac760723818badc674c33ce3137ec2901324ef
Rich Gibson <rich.gibson@gmail.com>
2014-02-27 00:05:18 +0100

    Filter residency opportunities shown on the organization page Fixes #20

--- page_processors.py
+++ page_processors.py
@@ -1,6 +1,7 @@
 #from django import forms
 #from django.http import HttpResponseRedirect
 
+import sys
 from mezzanine.pages.page_processors import processor_for
 from .models import Organization, Residency
 
@@ -10,7 +11,7 @@ from .models import Organization, Residency
 
 @processor_for(Organization)
 def organization_display(request, page):
-    reslist = Residency.objects.all()
+    reslist = Residency.objects.filter(organization=page.id)
     return {"reslist": reslist}
 
 @processor_for('list-of-organizations')
@@ -21,6 +22,10 @@ def organization_list(request,page):
 @processor_for('list-of-residencies')
 def residency_list(request,page):
     reslist = Residency.objects.all()
+    # this is where we could do the offer/requires formatting, but 
+    # I am doing it in a template_tag. 
+    #for res in reslist:
+    #    pass    
     return {"reslist": reslist}
 
 #### this is from the docs.http://mezzanine.jupo.org/docs/content-architecture.html
