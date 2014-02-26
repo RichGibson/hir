f033a5e1d4ce69eaa41bb7b4377c3608847a8785
Rich Gibson <rich.gibson@gmail.com>
2014-02-26 21:41:56 +0100

    Fixed checkboxes on the residency page. Fixes #18

--- templatetags/extras.py
+++ templatetags/extras.py
@@ -2,6 +2,58 @@ from django import template
 import sys
 register = template.Library()
 
+
+
+from django import template
+#@register.tag(name="check_magic")
+
+@register.simple_tag
+def tag1():
+    return "tag1 no parameter"
+
+@register.simple_tag
+def tag2(p):
+    return "tag2 one parameter: %s"  % p
+
+
+@register.simple_tag
+def check_magic(label, check, detail=None):
+    if check is True:
+        c="Checked"
+    else:
+        c=""
+
+    st = """<label class="control-label">%s</label> n/a<br>""" % label
+    if check is True or detail:
+        st = """ 
+        <label class="control-label">%s</label>
+        <input type="checkbox" disabled %s>
+        %s
+        <br>
+
+        """ % (label, c, detail)
+    return st
+
+
+
+# this won't work, I guess you can't pass this many things to a template filter
+@register.filter
+def Xmy_check(label, check, detail):
+    """ return pretty html which does the right thing in showing
+        or hiding the label, checkbox, and detail text based on
+        the values of the check and detail fields. If check is 
+        True, or there is anything in detail then show it. """
+
+    st = ""
+    if (check is True) or detail:
+        st="label, check, detail %s %s %s " % (label, check, detail)
+
+    return st
+
+
+# call this with something like this in the template:
+# entry is a dictionary
+# {{entry|get_item:'Name'}}
 @register.filter
 def get_item(dictionary, key):
     print >>sys.stderr, "----------------------"
